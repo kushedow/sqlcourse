16565ea40e01d6559571c1c0c203a94fcd562f62
Gleb Kushedow <kushedow@gmail.com>
2025-11-12 22:15:58 +0300

    fix hash router

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import Promo from './components/promo.vue'
 import Content_step from './components/content_step.vue'
@@ -22,7 +22,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes,
     scrollBehavior(to, from, savedPosition) {
         // Smooth scroll to top
